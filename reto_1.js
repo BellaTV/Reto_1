c9567fb5f99d48293c6cff11e3ea48bbafe94a07
isabelaTV <isabellactv96@gmail.com>
2024-10-29 21:09:59 -0500

    reto resuelto

--- reto_1.js
+++ reto_1.js
@@ -1,10 +1,9 @@
-const mensajeEncriptado = '^[()`~^*/?`[()^+`-~()#[$()/~()%\[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^\{()*~¡[$[{()¡[()?=[]`\¬]()¿()[{;+[$__~()`~]=\]+~'
+const mensajeEncriptado = '^[()`~^*/?`[()^+`-~()#[$()/~()%\\[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^\\{()*~¡[$[{()¡[()?=[]`\\¬]()¿()[{;+[$__~()`~]=\\]+~'
 
 const caracteres = {
-    '()' :' ',
     '?':'a',
     '[':'e',
-    '\\' : 'i', 
+    '\\': 'i',
     '~':'o',
     '+':'u',
     '¬':'ó',
@@ -31,7 +30,22 @@ const reemplazarPista = (mensaje, pista, reemplazo) => {
 const mensajeDesencriptado = desencriptarMensaje(mensajeEncriptado);
 console.log('Mensaje desencriptado:', mensajeDesencriptado);
 
-const intento2 = reemplazarPista(mensajeDesencriptado,'$[?`t', 'react' );
-const intento3 = reemplazarPista(intento2, 'g\=-+%', 'GitHub');
-const intento4 = reemplazarPista(intento3, '__', 'z' )
-console.log(intento4)
+const intento2 = reemplazarPista(mensajeDesencriptado,'$', 'r' );
+const intento3 = reemplazarPista(intento2, '%', 'b');
+const intento4 = reemplazarPista(intento3, '=', 't');
+const intento5 = reemplazarPista(intento4, '__', 'z' );
+const intento6 = reemplazarPista(intento5, '^', 'm');
+const intento7 = reemplazarPista(intento6, '-', 'h');
+const intento8 = reemplazarPista (intento7, '()', '  ');
+const intento9 = reemplazarPista (intento8, '{', 's');
+const intento10 = reemplazarPista (intento9, ']', 'n');
+const intento11 = reemplazarPista (intento10, '`', 'c');
+const intento12 = reemplazarPista (intento11, '*', 'p');
+const intento13 = reemplazarPista (intento12, '/', 'l');
+const intento14 = reemplazarPista (intento13, '!', 'j');
+const intento15 = reemplazarPista (intento14,';', 'f' );
+const intento16 = reemplazarPista (intento15, '¡', 'd');
+const intento17 = reemplazarPista (intento16, '¿', 'y');
+const intento18 = reemplazarPista (intento17, '}', 'q');
+const intento19 = reemplazarPista (intento18, '#', 'v');
+console.log(intento19);
